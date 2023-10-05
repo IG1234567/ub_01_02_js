599c59fc6cb27834a8116275a9bda08304fee068
IG1234567 <126507009+IG1234567@users.noreply.github.com>
2023-10-05 19:25:22 +0000

    commit

--- 0510 Voting machine/script.js
+++ 0510 Voting machine/script.js
@@ -18,45 +18,45 @@ document.addEventListener("DOMContentLoaded", function () {
             console.error("Nepavyko nuskaityti kandidatų duomenų:", error);
         });
 
-        form.addEventListener("submit", function (e) {
-            e.preventDefault();
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+    
+        const firstName = document.getElementById("firstName").value;
+        const lastName = document.getElementById("lastName").value;
+        const age = document.getElementById("age").value;
         
-            const firstName = document.getElementById("firstName").value;
-            const lastName = document.getElementById("lastName").value;
-            const age = document.getElementById("age").value;
-            
-            const selectedCandidateId = candidatesSelect.value;
+        const selectedCandidateId = candidatesSelect.value;
+    
+        // veiksmai su informacija
+        console.log("First Name:", firstName);
+        console.log("Last Name:", lastName);
+        console.log("Age:", age);
+        console.log("Selected Candidate ID:", selectedCandidateId);
         
-            // veiksmai su informacija
-            console.log("First Name:", firstName);
-            console.log("Last Name:", lastName);
-            console.log("Age:", age);
-            console.log("Selected Candidate ID:", selectedCandidateId);
-            
-            // Sukuriam objektą su balsu
-            const vote = {
-                firstName,
-                lastName,
-                age,
-                selectedCandidateId,
-            };
-        
-            // POST užklausą į serverį
-            fetch("/register-vote", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(vote),
+        // Sukuriam objektą su balsu
+        const vote = {
+            firstName,
+            lastName,
+            age,
+            selectedCandidateId,
+        };
+    
+        // POST užklausą į serverį
+        fetch("/register-vote", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(vote),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                // veiksmas po balsavimo
+                console.log("Balsas užregistruotas:", data);
             })
-                .then((response) => response.json())
-                .then((data) => {
-                    // veiksmas po balsavimo
-                    console.log("Balsas užregistruotas:", data);
-                })
-                .catch((error) => {
-                    console.error("Nepavyko užregistruoti balsu:", error);
-                });
+            .catch((error) => {
+                console.error("Nepavyko užregistruoti balsu:", error);
+            });
     });
 
     // Fetch the vote summary data from the server
@@ -66,14 +66,14 @@ document.addEventListener("DOMContentLoaded", function () {
             // Populate the HTML page with the vote summary data
             const summaryDiv = document.getElementById("summary");
 
-        // Display the data in the "summary" div or format it as needed
-        summaryDiv.innerHTML = `
-            <p>Candidate 1 Votes: ${data.candidate1Count} (${data.candidate1Percentage}%)</p>
-            <p>Candidate 2 Votes: ${data.candidate2Count} (${data.candidate2Percentage}%)</p>
-            <p>Candidate 3 Votes: ${data.candidate3Count} (${data.candidate3Percentage}%)</p>
-        `;
-    })
-.catch((error) => {
-    console.error("Failed to fetch vote summary:", error);
+            // Display the data in the "summary" div or format it as needed
+            summaryDiv.innerHTML = `
+                <p>Candidate 1 Votes: ${data.candidate1Count} (${data.candidate1Percentage}%)</p>
+                <p>Candidate 2 Votes: ${data.candidate2Count} (${data.candidate2Percentage}%)</p>
+                <p>Candidate 3 Votes: ${data.candidate3Count} (${data.candidate3Percentage}%)</p>
+            `;
+        })
+        .catch((error) => {
+            console.error("Failed to fetch vote summary:", error);
+        });
 });
-}
