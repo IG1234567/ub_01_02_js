694118ddeded8756bdcc937a7de0d041da1f760b
IG1234567 <126507009+IG1234567@users.noreply.github.com>
2023-10-05 18:58:39 +0300

    commit

--- 0510 Voting machine/script.js
+++ 0510 Voting machine/script.js
@@ -1,17 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("candidateForm");
+    const candidatesSelect = document.getElementById("candidates");
+
+    // Nuskaitom kandidatų sąrašą iš candidates.json failo
+    fetch("candidates.json")
+        .then((response) => response.json())
+        .then((data) => {
+            // Pildome pasirinkimo lauką kandidatų duomenimis
+            data.candidates.forEach((candidate) => {
+                const option = document.createElement("option");
+                option.value = candidate.id;
+                option.text = candidate.name;
+                candidatesSelect.appendChild(option);
+            });
+        })
+        .catch((error) => {
+            console.error("Nepavyko nuskaityti kandidatų duomenų:", error);
+        });
+
     form.addEventListener("submit", function (e) {
         e.preventDefault();
 
         const firstName = document.getElementById("firstName").value;
         const lastName = document.getElementById("lastName").value;
         const age = document.getElementById("age").value;
-        const candidate = document.getElementById("candidates").value;
+        const selectedCandidateId = candidatesSelect.value;
 
-        // I can use these values as needed (e.g., display them, send them to the backend, etc.)
+        // veiksmai su informacija
         console.log("First Name:", firstName);
         console.log("Last Name:", lastName);
         console.log("Age:", age);
-        console.log("Selected Candidate:", candidate);
+        console.log("Selected Candidate ID:", selectedCandidateId);
     });
 });
