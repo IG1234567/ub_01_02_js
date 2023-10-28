0bbe33206fe0f30c4ccd9722291b83129395bdb8
IG1234567 <126507009+IG1234567@users.noreply.github.com>
2023-10-28 13:07:52 +0300

    commit

---
+++ _FINAL_PROJECT /Frontend/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ProductList from './ProductList';
+import ShoppingCart from './ShoppingCart';
+import UserAccount from './UserAccount';
+import Navigation from './Navigation';
+
+function App() {
+  return (
+    <Router>
+      <div>
+      <Navigation />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/shopping-cart" component={ShoppingCart} />
+          <Route path="/user-account" component={UserAccount} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
--- _FINAL_PROJECT/Backend/express.js
+++
@@ -1,4 +0,0 @@
-app.listen(port, () => {
-    console.log(`Serveris veikia adresu http://localhost:3000`);
-  });
-  
